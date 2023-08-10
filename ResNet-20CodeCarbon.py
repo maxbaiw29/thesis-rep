9b15c925f549e01ca9594f794fddf118f4172a0e
maxbaiw29 <124596082+maxbaiw29@users.noreply.github.com>
2023-08-10 14:39:36 +0200

    Add files via upload

---
+++ ResNet-20CodeCarbon.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from codecarbon import EmissionsTracker
+
+
+# Load the MNIST dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Preprocess the data
+x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.0
+x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.0
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+
+# Define the model
+inputs = keras.Input(shape=(28, 28, 1))
+x = layers.Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+
+# Convolutional block 1
+activation = 'relu'
+shortcut = x
+x = layers.Conv2D(filters=32, kernel_size=3, strides=1, padding='same')(x)
+x = layers.Activation(activation)(x)
+shortcut = layers.Conv2D(filters=32, kernel_size=3, strides=1, padding='same')(shortcut)# Adjusted shortcut
+x = layers.add([x, shortcut])
+x = layers.Activation(activation)(x)
+
+
+# Convolutional block 2
+shortcut = x
+x = layers.Conv2D(filters=64, kernel_size=3, strides=1, padding='same')(x)
+x = layers.Activation(activation)(x)
+shortcut = layers.Conv2D(filters=64, kernel_size=3, strides=1, padding='same')(shortcut)# Adjusted shortcut
+x = layers.add([x, shortcut])
+x = layers.Activation(activation)(x)
+
+# Convolutional block 3
+shortcut = x
+x = layers.Conv2D(filters=156, kernel_size=3, strides=1, padding='same')(x)
+x = layers.Activation(activation)(x)
+shortcut = layers.Conv2D(filters=156, kernel_size=3, strides=1, padding='same')(shortcut)# Adjusted shortcut
+x = layers.add([x, shortcut])
+x = layers.Activation(activation)(x)
+
+
+# Global Average Pooling
+x = layers.GlobalAveragePooling2D()(x)
+
+# Fully-connected layer
+x = layers.Dense(256, activation='relu')(x)
+
+# Softmax activation
+outputs = layers.Dense(10, activation='softmax')(x)
+
+# Create the model
+model = keras.Model(inputs=inputs, outputs=outputs)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#CodeCarbon - tracker
+tracker = EmissionsTracker()
+tracker.start()
+
+# Train the model
+model.fit(x_train, y_train, batch_size=128, epochs=1, validation_data=(x_test, y_test))
+
+emissions = tracker.stop()
+print(f"Emissions: {emissions} kg")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
